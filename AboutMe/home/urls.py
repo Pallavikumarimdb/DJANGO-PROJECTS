851ab1f1ad1463bd3a7a9e8abd86e367804486f3
Pallavi <pallavilpmt1995@gmail.com>
2023-05-15 13:38:59 +0530

    Add pages

--- AboutMe/home/urls.py
+++ AboutMe/home/urls.py
@@ -4,5 +4,8 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
-    path("", views.index, name='home')
+    path("", views.index, name='home'),
+    path("about", views.about, name='about'),
+    path("services", views.services, name='services'),
+    path("contact", views.contact, name='contact')
 ]
--- AboutMe/home/views.py
+++ AboutMe/home/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return HttpResponse("This is home Page")
+
+def about(request):
+    return HttpResponse("This is about page")
+
+def services(request):
+    return HttpResponse("This is services page")
+
+def contact(request):
+    return HttpResponse("This is contact page")
